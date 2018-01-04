bd74b4466f7e274072c15f4023e3664b28c0a869
chadl76 <chad.lucas.19@gmail.com>
2018-01-04 13:39:32 -0800

    add box shadow to post

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,15 @@
+//const contain = document.querySelector('.side-links');
+//console.log(contain)
+
+//console.log(document);
+
+//contain.addEventListener("click", function(e){
+//	console.log(e);
+//} )
+
+window.onload = function() {
+	 const dommy = document.querySelector('.post-form');
+	 dommy.addEventListener("click", e => {
+	 	console.log(e)
+	 })
+}
